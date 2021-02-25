93dd7703c285b7f1829df4c311b85f22246354dd
zirpin <zirpin@gmail.com>
2021-02-25 16:40:27 +0100

    initial commit for the feature stage2

---
+++ src/test/java/com/patterns/prueba/TestJavier/State2Test.java
@@ -0,0 +1,19 @@
+package com.patterns.prueba.TestJavier;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.patterns.behavioral.state.state2.Device;
+import com.patterns.behavioral.state.state2.Off;
+import com.patterns.behavioral.state.state2.On;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class State2Test {
+
+
+
+
+}
