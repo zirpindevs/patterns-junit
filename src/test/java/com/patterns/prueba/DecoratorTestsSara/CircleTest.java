36032b546f1c115daed959550a8093928f2939a4
Sara <sbr.90@hotmail.com>
2021-02-25 16:32:09 +0100

    decorator tests

---
+++ src/test/java/com/patterns/prueba/DecoratorTestsSara/CircleTest.java
@@ -0,0 +1,28 @@
+package com.patterns.prueba.DecoratorTestsSara;
+
+import com.patterns.structural.decorator.Circle;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CircleTest {
+
+   /* @Test
+    void draw() {
+    }
+
+    @Test
+    void resize() {
+    }*/
+
+    @Test
+    void description() {
+        Circle circle = new Circle();
+        assertEquals("Hey there! I'm using Decorator!", circle.description());
+    }
+
+    @Test
+    void isHide() {
+        Circle circle = new Circle();
+        assertEquals(false, circle.isHide());
+    }
+}
