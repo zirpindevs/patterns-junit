3d111cc371fe12bd1e2f086066b0d23c1f486392
Monica Lara <ptts.no2020@gmail.com>
2021-02-25 16:35:52 +0100

    #commit2 WeatherTestCrearWather

--- src/test/java/com/patterns/behavioral/observer/observer1/WeatherTest.java
+++ src/test/java/com/patterns/behavioral/observer/observer1/WeatherTest.java
@@ -1,10 +1,35 @@
 package com.patterns.behavioral.observer.observer1;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WeatherTest {
+    private WeatherType currentWeather;
+    private List<WeatherObserver> observers;
+
+    @Test
+    @BeforeEach
+    void comienzo(){
+        System.out.println("comienzo");
+        //para crear los casos de test por cada llamada se limpiará la lista de observadores
+        observers = new ArrayList<>();
+
+        WeatherObserver computer = new Computer();
+        WeatherObserver smartphone = new Smartphone();
+    }
+    @Test
+    @DisplayName("Instancia Weather con lista vacia")
+    void crearWeather(){
+        Weather weather = new Weather();
+        assertTrue(observers.size()==0);
+
+    }
 
     @Test
     void addObserver() {
