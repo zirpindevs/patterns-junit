8e6324a2411a821d72460086cc2b29fea58cc59c
Juanma <juanmanuelruizgil@gmail.com>
2021-02-25 16:50:34 +0100

    Task 1 - Test apply to ProductTest

--- src/test/java/com/patterns/behavioral/strategy/strategy1/CreditCardStrategyTest.java
+++ src/test/java/com/patterns/behavioral/strategy/strategy1/CreditCardStrategyTest.java
@@ -1,7 +1,59 @@
 package com.patterns.behavioral.strategy.strategy1;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreditCardStrategyTest {
 
+    /*
+    *   We are going to check CVV code has a length of 3.
+    * */
+    @Test
+    @DisplayName("check CVV has a lenght of 3.")
+    void test1(){
+        CreditCardStrategy card = new CreditCardStrategy("Juan", "456785413658","456","25/02/2007");
+
+        Function<String,Integer> calc = String::length;
+        assertEquals(3, calc.apply(card.getCcv()));
+    }
+
+    @Test
+    @DisplayName("check CVV code is digits")
+     void test2(){
+        CreditCardStrategy card = new CreditCardStrategy("Juan", "456785413658","456","25/02/2007");
+        assertTrue(card.getCcv().chars().allMatch(Character::isDigit));
+    }
+    @Test
+    @DisplayName("check cardNumber is only digit")
+    void test3(){
+        CreditCardStrategy card = new CreditCardStrategy("Juan", "456785413658","456","25/02/2007");
+        assertTrue(card.getCardNumber().chars().allMatch(Character::isDigit));
+    }
+
+    @Test
+    @DisplayName("DateOfExpirity is a valid DATE")
+    void test4() throws ParseException {
+        CreditCardStrategy card = new CreditCardStrategy("Juan", "456785413658","456","25-02-2007");
+        assertTrue(isDateValid(card.getDateOfExpiry()));
+
+    }
+
+    public static boolean isDateValid(String date)
+    {
+        try {
+            DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+            df.setLenient(false);
+            df.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
 }
---
+++ src/test/java/com/patterns/behavioral/strategy/strategy1/ProductTest.java
@@ -0,0 +1,20 @@
+package com.patterns.behavioral.strategy.strategy1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.DoubleBinaryOperator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    @DisplayName("Check, Product instance created is not null")
+    void test1(){
+        Product product = new Product("10845",25);
+        assertNotNull(product);
+    }
+
+
+}
