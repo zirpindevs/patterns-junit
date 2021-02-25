74303e24019e4209a01035ba822c2c63e45355c1
Adrián Núñez <adrinunezmunoz@gmail.com>
2021-02-25 16:58:15 +0100

    builder2Test WIP

---
+++ src/test/java/com/patterns/prueba/builder2Test.java
@@ -0,0 +1,20 @@
+package com.patterns.prueba;
+import com.patterns.creational.builder.builder2.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class builder2Test {
+
+    Cocina cocina = new Cocina();
+    HamburguesaBuilder yankeeBuilder = new HamburguesaYankeeBuilder();
+    HamburguesaBuilder cangreBuilder = new HamburguesaCangreburgerBuilder();
+
+    @Test
+    void HamburgesaBuilderTest() {
+/**
+ * -------------------------WIP----------------------------
+ */
+    }
+}
