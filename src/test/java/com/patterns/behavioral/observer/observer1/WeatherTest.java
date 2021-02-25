09b9e0da60809c361c7b39e0c40542bd7320b9e8
Monica Lara <ptts.no2020@gmail.com>
2021-02-25 16:22:47 +0100

    #commit WeatherTest1

---
+++ src/test/java/com/patterns/behavioral/observer/observer1/WeatherTest.java
@@ -0,0 +1,20 @@
+package com.patterns.behavioral.observer.observer1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherTest {
+
+    @Test
+    void addObserver() {
+    }
+
+    @Test
+    void removeObserver() {
+    }
+
+    @Test
+    void changeWeather() {
+    }
+}
