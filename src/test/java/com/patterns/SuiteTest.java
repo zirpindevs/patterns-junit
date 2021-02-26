d4a448ba4501c1fc92388eacdd9c44b04aefd9c1
scarrasco85 <chechucarrasco@gmail.com>
2021-02-26 20:34:56 +0100

    preparying iterator mockito test

--- src/test/java/com/patterns/SuiteTest.java
+++ src/test/java/com/patterns/SuiteTest.java
@@ -1,23 +1,22 @@
 package com.patterns;
 
-
-import com.patterns.behavioral.strategy.strategy1.CreditCardStrategyTest;
 import com.patterns.behavioral.strategy.strategy1.PaypalStrategyTest;
 import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
-//SelectPackages({"com.patterns.creational", "com.patterns.behavioral"})
-@SelectPackages("com.patterns")
-@SuiteDisplayName("Suite patrones")
 
-//@IncludeTags("database")
-/*@IncludeTags({
-        "database",
-        "service"
-})*/
-// @ExcludeTags("webmvc")
-// @SelectClasses({PaypalStrategyTest.class, CreditCardStrategyTest.class})
+/*
+ Ejecutaría todos los test desde esta clase que estén dentro del paquete creational e iterator y que  la
+ clase o método contenga la Tag "database"
+* */
+@RunWith(JUnitPlatform.class)
+@SelectPackages({"com.patterns.creational", "com.patterns.iterator"})
+@SuiteDisplayName("Suite patrones")
+@IncludeTags("database") // También se puede usar @ExcludeTags para el caso contrario
+@SelectClasses(PaypalStrategyTest.class)
 public class SuiteTest {
 }
