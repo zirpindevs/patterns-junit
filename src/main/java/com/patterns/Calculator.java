e5c6bd0e855021f2d37990e9c20604989dfad90b
Alan Sastre <alan@alansastre.co>
2021-03-02 18:21:01 +0100

    added maven jacoco testing coverage

--- src/main/java/com/patterns/Calculator.java
+++ src/main/java/com/patterns/Calculator.java
@@ -2,7 +2,14 @@ package com.patterns;
 
 public class Calculator {
 
+    /**
+     * It multiplies two numbers
+     * @param num1
+     * @param num2
+     * @return
+     */
     public double multiply(double num1, double num2){
+        // multiply two doubles
         return num1 * num2;
     }
 }
