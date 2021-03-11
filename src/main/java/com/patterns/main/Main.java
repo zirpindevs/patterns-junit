3906991eba3541176813a4e691fb2dabb7ea4fc2
zirpin <zirpin@gmail.com>
2021-03-11 13:28:42 +0100

    add log

--- src/main/java/com/patterns/main/Main.java
+++ src/main/java/com/patterns/main/Main.java
@@ -1,9 +1,15 @@
 package com.patterns.main;
+import com.patterns.behavioral.iterator.iterator1.BookShop;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+
+        final Logger log = LoggerFactory.getLogger(BookShop.class);
+
+        String logMessage = "Hola mundo!";
+        log.info(logMessage);
     }
 }
