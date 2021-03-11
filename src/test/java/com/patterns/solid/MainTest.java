a6604d25bb47c4116f2210e3a62b7f1257adb3ec
zirpin <zirpin@gmail.com>
2021-03-11 12:30:14 +0100

    create testconstructor

--- src/test/java/com/patterns/solid/MainTest.java
+++ src/test/java/com/patterns/solid/MainTest.java
@@ -1,14 +1,17 @@
 package com.patterns.solid;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
 
-   // @Test
-
-//Cambio2
-// Cambio de carpeta
-
-
-
+    @Test
+    @DisplayName("Test car not null")
+    public void testConstructor()
+    {
+        ICar car;
+        car = new ElectricCar();
+        assertNotNull(car);
+    }
 }
