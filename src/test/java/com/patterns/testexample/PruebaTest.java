2fc8fd6d551cd1a061a4cfa5d4e7b3d944bc9b53
zirpin <zirpin@gmail.com>
2021-03-11 12:38:20 +0100

    add a string test

--- src/test/java/com/patterns/testexample/PruebaTest.java
+++ src/test/java/com/patterns/testexample/PruebaTest.java
@@ -1,4 +1,17 @@
 package com.patterns.testexample;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class PruebaTest {
+    @Test
+    @DisplayName("Test name")
+    public void testName()
+    {
+        String myName ="Javier";
+        assertNotEquals("Javi",myName);
+
+    }
 }
+
