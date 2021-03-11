a69adc229de9646b78a276bf77ce66bbfa645713
zirpin <zirpin@gmail.com>
2021-03-11 12:47:32 +0100

    add a dummy string test

--- src/test/java/com/patterns/mockito/SuiteTest.java
+++ src/test/java/com/patterns/mockito/SuiteTest.java
@@ -1,6 +1,18 @@
 package com.patterns.mockito;
 
-public class SuiteTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-}
+class SuiteTest {
+
+    @Test
+    @DisplayName("Test dummy name")
+    void testDummyName()
+    {
+        String dummyName ="mockitoSuite";
+        assertNotEquals("mockito",dummyName);
+
+    }
+    }
